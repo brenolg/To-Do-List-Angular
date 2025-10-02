75d5d9a1c4f28fef1a1d86c130937f6c6ae36748
brenolg <brenolgar@gmail.com>
2025-10-02 17:51:14 -0300

    angular projcect fisish

--- src/app/modules/home/components/to-do-input-add-itens/to-do-input-add-itens.ts
+++ src/app/modules/home/components/to-do-input-add-itens/to-do-input-add-itens.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-input-add-itens',
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './to-do-input-add-itens.html',
   styleUrl: './to-do-input-add-itens.scss'
 })
-export class ToDoInputAddItens {
+export class ToDoInputAddItens implements OnInit  {
 
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+  };
 }
